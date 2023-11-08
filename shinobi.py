ad5fc25776f18c2c3814040dabec91f0f13347ff
Xenomii <voltzxp@gmail.com>
2023-11-08 18:39:05 +0800

    Write outputs to file

--- shinobi.py
+++ shinobi.py
@@ -5,11 +5,18 @@ import shodan
 import time
 import json
 import configparser as ConfigParser
+from datetime import datetime
 
 # Get the configparser object
 config = ConfigParser.ConfigParser()
 
-CVE_LOOKUP = False
+def get_unique_filename():
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d_%H%M%S")
+    return f"results_{timestamp}.txt"
+
+FILENAME = 'results/' + get_unique_filename()
+CVELOOKUP = True
 
 def check_ssl_certificate(domain):
     try:
@@ -25,33 +32,33 @@ def check_ssl_certificate(domain):
             result = response.json()
 
         print("SSL certificate assessment completed.")
-        print(f"Overall grade: {result['endpoints'][0]['grade']}")
+        with open(FILENAME, 'a') as file:
 
-        # Print more detailed information about the SSL certificate
-        endpoints = result.get('endpoints', [])
-        for endpoint in endpoints:
-            print(f"\nServer: {endpoint.get('serverName', 'N/A')}")
-            print(f"Grade: {endpoint.get('grade', 'N/A')}")
-            print(f"IP Address: {endpoint.get('ipAddress', 'N/A')}")
-            details = endpoint.get('details', {})
-            
-            protocols = details.get('protocols', [])
-            if protocols:
-                print("TLS Version(s):")
-                for x in range(len(protocols)):
-                    print(protocols[x].get('version'))
-            else:
-                print("Protocol: N/A")
-            
-            suites = details.get('suites', [])
-            if suites:
-                print('Cipher Suites:')
-                for x in range(len(suites)):
-                    slist = suites[x].get('list', [])
-                    for y in range(len(slist)):
-                        print(slist[y].get('name'))
-            else:
-                print("Cipher Suite: N/A")
+            #  more detailed information about the SSL certificate
+            endpoints = result.get('endpoints', [])
+            for endpoint in endpoints:
+                file.write(f"\n\nServer: {endpoint.get('serverName', 'N/A')}")
+                file.write(f"\nGrade: {endpoint.get('grade', 'N/A')}")
+                file.write(f"\nIP Address: {endpoint.get('ipAddress', 'N/A')}")
+                details = endpoint.get('details', {})
+                
+                protocols = details.get('protocols', [])
+                if protocols:
+                    file.write("\nTLS Version(s): ")
+                    for x in range(len(protocols)):
+                        file.write("\n" + protocols[x].get('version'))
+                else:
+                    file.write("\nProtocol: N/A")
+                
+                suites = details.get('suites', [])
+                if suites:
+                    file.write("\n\nCipher Suites:")
+                    for x in range(len(suites)):
+                        slist = suites[x].get('list', [])
+                        for y in range(len(slist)):
+                            file.write("\n" + slist[y].get('name'))
+                else:
+                    file.write("\nCipher Suite: N/A")
 
     except requests.exceptions.RequestException as e:
         print(f"Request error: {e}")
@@ -80,37 +87,43 @@ def use_shodan(ip):
         api = shodan.Shodan(SHODAN_API_KEY)
         host_info = api.host(ip)
 
-        print(f"Organization: {host_info.get('org', 'N/A')}")
-        print(f"Operating System: {host_info.get('os', 'N/A')}")
+        with open(FILENAME, "a") as file:        
+            file.write(f"\nOrganization: {host_info.get('org', 'N/A')}")
+            file.write(f"\nOperating System: {host_info.get('os', 'N/A')}")
 
-        print("Open Ports:")
-        for item in host_info['data']:
-            port = item['port']
-            service = item.get('banner', 'N/A')
-            print(f"Port: {port} - Service: {service}")
+            file.write("\n\nOpen Ports:")
+            for item in host_info['data']:
+                port = item['port']
+                service = item.get('banner', 'N/A')
+                file.write(f"\nPort: {port} - Service: {service}")
 
-        print("Vulnerabilities:")
-        vuln_info = host_info.get('vulns', 'No vulnerabilities found')
-        print(vuln_info)
+            file.write("\n\nVulnerabilities:\n")
+            vuln_info = host_info.get('vulns', 'No vulnerabilities found')
+            print(vuln_info)
 
-        if vuln_info != 'No vulnerabilities found' and CVE_LOOKUP == True:
-            request_count = 0
-            for cve_id in vuln_info:
-                result = lookup_cve(cve_id)
-                request_count += 1
+            if vuln_info != 'No vulnerabilities found' and CVELOOKUP == True:
+                print("Collecting details on identified CVEs...")
+                request_count = 0
+                for cve_id in vuln_info:
+                    result = lookup_cve(cve_id)
+                    request_count += 1
 
-                if request_count % 5 == 0:
-                    time.sleep(40)
+                    if request_count % 5 == 0:
+                        time.sleep(40)
 
-                if "result" in result:
-                    cve_data = result["result"]["CVE_Items"][0]
-                    description = cve_data["cve"]["description"]["description_data"][0]["value"]
-                    cve_url = cve_data["cve"]["references"]["reference_data"][0]["url"]
-                    print(f"CVE ID: {cve_id}")
-                    print(f"Description: {description}")
-                    print(f"URL: {cve_url}\n")
-                else:
-                    print(f"Error looking up CVE {cve_id}: {result}")
+                    if "result" in result:
+                        cve_data = result["result"]["CVE_Items"][0]
+                        description = cve_data["cve"]["description"]["description_data"][0]["value"]
+                        cve_url = cve_data["cve"]["references"]["reference_data"][0]["url"]
+                        cwe = cve_data["cve"]["problemtype"]["problemtype_data"][0]["description"][0]["value"]
+                        cvss = cve_data.get("impact", {}).get("baseMetricV3", {}).get("cvssV3", {}).get("baseScore")
+                        file.write("\nCVE ID: " + cve_id + "\n")
+                        file.write("Description: " + description + "\n")
+                        file.write("URL: " + cve_url + "\n")
+                        file.write("CWE: " + cwe + "\n")
+                        file.write(f"CVSS Base Score: {cvss}\n")
+                    else:
+                        print(f"Error looking up CVE {cve_id}: {result}")
 
     except shodan.APIError as e:
         print(f"Error: {e}")
@@ -126,9 +139,11 @@ def passive_ip_lookup(domain):
         response = requests.get(ipinfo_url)
         ipinfo_data = response.json()
 
-        print(f"City: {ipinfo_data.get('city', 'N/A')}")
-        print(f"Region: {ipinfo_data.get('region', 'N/A')}")
-        print(f"Country: {ipinfo_data.get('country', 'N/A')}")
+        with open(FILENAME, "a") as file:
+            file.write(f"\nCity: {ipinfo_data.get('city', 'N/A')}")
+            file.write(f"\nRegion: {ipinfo_data.get('region', 'N/A')}")
+            file.write(f"\nCountry: {ipinfo_data.get('country', 'N/A')}")
+            file.write(f"\nISP: {ipinfo_data.get('org', 'N/A')}")
     except socket.gaierror:
         print(f"Unable to resolve the IP address for '{domain}'")
     except requests.exceptions.RequestException:
@@ -138,8 +153,7 @@ def passive_ip_lookup(domain):
 def passive_subdomain_enum(domain):
     sources = [
         "https://crt.sh/?q=%.{0}&output=json".format(domain),
-        "https://certspotter.com/api/v0/certs?domain={0}".format(domain),
-        # Add more data sources here
+        "https://certspotter.com/api/v0/certs?domain={0}".format(domain)
     ]
 
     subdomains = set()
@@ -154,24 +168,23 @@ def passive_subdomain_enum(domain):
         except Exception as e:
             print(f"Error while querying {source}: {e}")
 
-    return subdomains
+    if subdomains:
+        with open(FILENAME, 'a') as file:
+            file.write("\n\nSubdomains found:\n")
+            for subdomain in subdomains:
+                file.write(subdomain)
+    else:
+        print("No subdomains found for the provided domain.")
 
 if __name__ == "__main__":
     target_domain = input("Enter the target domain: ")
-    
+    print("Collecting IP information...")
     ip_add = passive_ip_lookup(target_domain)
-
+    print("Collecting port information and determining vulnerabilities...")
     use_shodan(ip_add)
-    
+    print("Collecting & assessing SSL certificate...")
     check_ssl_certificate(target_domain)
-
-    subdomains = passive_subdomain_enum(target_domain)
-
-    if subdomains:
-        print("\nSubdomains found:")
-        for subdomain in subdomains:
-            print(subdomain)
-    else:
-        print("No subdomains found for the provided domain.")
+    print("Finding subdomains that can increase attack vector...")
+    passive_subdomain_enum(target_domain)
 
     
